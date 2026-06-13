c607b992ea668083e4dc42d59abec06f442e40d9
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-13 17:24:17 +0545

    learned about classList

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-// first step select the element
-const btn = document.querySelector('button');
-
-// btn.textContent = 'starting ...';
-
-// textContent and innerHTML
 const h1 = document.querySelector('h1');
-h1.textContent +='this text looks better';
 
-h1.innerHTML+='<i>. Hello</i>'
+h1.style.color = 'red'
+h1.style.fontFamily = 'Arial, sans-serif'
+h1.style.fontSize = '34px'
+h1.style.background = 'coral'
+
+// classlist
+
+const h2 = document.querySelector('h2');
+h2.classList.add('red')
+h2.classList.remove('red')
+h2.classList.toggle('red') // add if not present, remove if present
