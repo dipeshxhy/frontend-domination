1cb68badd9c9b2b2dc35f20fa132b5fd8ef93268
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-13 17:17:38 +0545

    learn about textcontent and innerHTML

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-//1. selection
-const button = document.querySelector('button');
-const downloadText= document.querySelector('p')
+// first step select the element
+const btn = document.querySelector('button');
 
+// btn.textContent = 'starting ...';
 
-console.log(button.textContent)
-button.addEventListener('click', () => {
-button.innerHTML ='Download'
-let count = 0;
-const interval = setInterval(() => {
-    count++;
-    downloadText.textContent = `Downloading... ${count}%`
-    if(count === 100){
-        clearInterval(interval);
-        downloadText.textContent = 'Download complete'
-    }
-}, 100);
-})
+// textContent and innerHTML
+const h1 = document.querySelector('h1');
+h1.textContent +='this text looks better';
+
+h1.innerHTML+='<i>. Hello</i>'
