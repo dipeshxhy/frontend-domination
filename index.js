de3f746afa7b12308859435e1934efc5af9b9ca5
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-13 20:59:55 +0545

    challenge 7 progressbar done

--- index.js
+++ index.js
@@ -1,23 +1,12 @@
-const tab = document.querySelector('.tabs')
-const texts = document.querySelectorAll('.texts p')
-console.log(texts)
-hideAll()
-texts[0].style.display = 'block'
-texts[0].style.width = '50%'
-tab.addEventListener('click',(e)=>{
-  console.log(e.target)
-  hideAll()
-  if(e.target.id==='home'){
-    texts[0].style.display = 'block'
-  }else if(e.target.id==='about'){
-    texts[1].style.display = 'block'
-  }else if(e.target.id==='contact'){
-    texts[2].style.display = 'block'
-  }
-})
-
-function hideAll(){
-  for(let elem of texts){
-    elem.style.display = 'none'
-  }
-}
+const progressTrack = document.querySelector('.progress');
+const textProgress = document.querySelector('.textProgress');
+let count = [10,20,30,40,50,60,70,80,90,100];
+const intervalId = setInterval(() => {
+  const value = count.shift();
+  progressTrack.style.width = `${value}%`;
+  progressTrack.innerHTML = `${value}%`;
+  textProgress.innerHTML = `downloading ${value}%`;
+  if (count.length === 0) {
+    textProgress.innerHTML = `download completed :)`;
+    clearInterval(intervalId)};
+}, 1000);
