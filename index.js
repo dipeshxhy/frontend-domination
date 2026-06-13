f82068c75a90c9c0b607d31b74789802c0704642
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-13 19:58:19 +0545

    done challenge 4

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 const form = document.querySelector('form')
 const inp1 = document.querySelector('#inp1')  
-const inp2 = document.querySelector('#inp2')
+const removeBtn= document.querySelector('#removeBtn')
+const lis = document.querySelectorAll('li')
+
 
 
 form.addEventListener('submit',(e)=>{
   e.preventDefault();
   let inp1Value = inp1.value;
-  let inp2Value = inp2.value;
-  if(!inp1Value || !inp2Value){
-    alert('Please fill both the inputs')
+  if(!inp1Value){
+    alert('Please fill the input')
   }else{
-    console.log(inp1Value, inp2Value)
+    const li = document.createElement('li');
+    li.textContent = inp1Value;
+    document.querySelector('ul').appendChild(li);
+    inp1.value = '';
   }
+})
+
+removeBtn.addEventListener('click', (e)=>{
+  e.preventDefault();
+  console.log(lis)
+ 
+  const lastLi = document.querySelector('ul li:last-child');
+  if(lastLi){
+    lastLi.remove();
+  }
+  
 })
