147b38280a4b259c742262abf489cbe4ffc7156d
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-13 18:00:56 +0545

    challenge3 done

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
+const img1 = document.querySelector('#img1');
+const img2 = document.querySelector('#img2');
 const btn = document.querySelector('button');
-const pText = document.querySelector('p');
-btn.addEventListener('click',(e)=>{
-pText.textContent = "Nice, Hello I am Dipesh"
-btn.textContent = 'Changed!'
-})
+const src1 = img1.getAttribute('src');
+const src2 = img2.getAttribute('src');
+
+let isToggle = false;
+btn.addEventListener('click', function() {
+  if(!isToggle) {
+    img1.setAttribute('src', src2);
+    img2.setAttribute('src', src1);
+  } else {
+    img1.setAttribute('src', src1);
+    img2.setAttribute('src', src2);
+  }  
+  isToggle = !isToggle;
+});
+   
