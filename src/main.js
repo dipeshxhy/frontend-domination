531dfbf20b814039da481dca36af8712136392b7
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-14 09:09:19 +0545

    learned about scroll trigger

--- src/main.js
+++ src/main.js
@@ -1,35 +1,45 @@
-import { gsap } from 'gsap'
-const tl = gsap.timeline({
-  defaults:{
-    duration:1,
-    ease:'power4.in'
+import { gsap } from "gsap";
+    
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+const tl = gsap.timeline()
+
+tl.from('.page1 .circle',{
+  scale:0,
+  opacity: 0,
+  rotate:760,
+  duration:4,
+ 
+
+})
+tl.from('.page2 .circle',{
+  scale:0,
+  opacity: 0,
+  rotate:360,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.page2 .circle',
+    scroller:'body',
+    start:'top 80%',
+    end:'top 40%',
+    scrub:1,
+    
+    markers:true,
+  }
+
+})
+gsap.from('.page3 .circle',{
+  scale:0,
+  opacity: 0,
+  rotate:360,
+  duration:2,
+  scrollTrigger:{
+    trigger:'.page3 .circle',
+    scroller:'body',
+    start:'top 80%',
+    end:'top 40%',
+    scrub:1,
+    markers:true,
   }
 })
-tl.from('.nav div, .nav li',{
-  y:-100,
-  duration:2,
-  opacity:0,
-  // delay:0.4,
-  stagger:0.1,
-  ease:'power4.out'
-})
-
-// main title animation
-tl.from('.title h1',{
-  x:-1500,
-  // opacity:0
-  stagger:0.3,
-  ease:'elastic.out(1,0.5)'
-})
-
-// images animation
-
-
-tl.from('.images img',{
-  x:100,
-  opacity:0,
-  stagger:1,
-  rotate:45,
-  ease:'power1.out'
-},'-=1.5')
-
