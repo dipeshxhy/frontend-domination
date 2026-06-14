b680f9e72c1e2059577f4e36a0607adf1177e6c7
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-14 18:43:29 +0545

    conditional rendering done

--- src/Card.jsx
+++ src/Card.jsx
@@ -6,14 +6,14 @@ const Card = ({ name, description, image,isFollow,onFollow,id }) => {
     onFollow(id);
   }
   return (
-    <div className="w-80 rounded  bg-slate-200">
-      <div className="w-full h-65 ">
-        <img className="w-full h-full rounded  object-cover" src={image} alt="" />
+    <div className="w-98 rounded flex p-4  bg-slate-200">
+      <div className=" flex-1  ">
+        <img className="w-full h-20 rounded  object-cover" src={image} alt="" />
       </div>
-      <div className="p-2">
+      <div className="p-2 flex-2">
         <h2 className="text-xl font-bold uppercase">{name}</h2>
         <p className="text-gray-600">{description}</p>
-        <button className="px-3.5 py-2 bg-blue-500 rounded text-white mt-4">Know more</button>
+        <button className="px-3.5 py-2 bg-blue-500 rounded text-white mt-4" onClick={handleFollow}>{isFollow ? 'Downloaded' : 'Download'}</button>
         <button className={`px-3.5 py-2 rounded text-white mt-4 ${isFollow ? 'bg-red-500' : 'bg-green-500'}`} onClick={handleFollow}>
           {isFollow ? 'Unfollow' : 'Follow'}
         </button>
