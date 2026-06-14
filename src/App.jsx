f613bd3090591044750fd63c48cd48484a245d22
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-14 21:25:14 +0545

    apply image slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,65 @@
-import { useState } from "react"
-import Card from "./Card"
-import Intermediate from "./Intermediate"
-import Advanced from "./Advanced"
+import { useState } from 'react';
+import Card from './Card';
+import ImageCard from './ImageCard';
 
-const data = [{
-  id:1,
-  name:'Dipesh Chaudhary',
-  description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.',
-  image:'https://images.unsplash.com/photo-1773332611612-ffdaa753afb1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3MXx8fGVufDB8fHx8fA%3D%3D',
-  isFollow:true
-},{
-  id:2,
-  name:'Manisha Kumari',
-  description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.',
-  image:'https://images.unsplash.com/photo-1773332611573-5e5bfa8e5de5?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3OHx8fGVufDB8fHx8fA%3D%3D',
-  isFollow:false
-},{
-  id:3,
-  name:'Sumit Kadel',
-  description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.',
-  image:'https://plus.unsplash.com/premium_photo-1779372801181-0d00b1448662?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMDJ8fHxlbnwwfHx8fHw%3D' ,
-  isFollow:true
-}]
+const data = [
+  {
+    id: 1,
+    name: 'Dipesh Chaudhary',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.',
+    image:
+      'https://images.unsplash.com/photo-1773332611612-ffdaa753afb1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3MXx8fGVufDB8fHx8fA%3D%3D',
+    isFollow: true,
+  },
+  {
+    id: 2,
+    name: 'Manisha Kumari',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.',
+    image:
+      'https://images.unsplash.com/photo-1773332611573-5e5bfa8e5de5?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3OHx8fGVufDB8fHx8fA%3D%3D',
+    isFollow: false,
+  },
+  {
+    id: 3,
+    name: 'Sumit Kadel',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.',
+    image:
+      'https://plus.unsplash.com/premium_photo-1779372801181-0d00b1448662?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMDJ8fHxlbnwwfHx8fHw%3D',
+    isFollow: true,
+  },
+];
 
 const App = () => {
-  const [value,setValue] = useState(data)
-  const [count,setCount] = useState(0)
-  const [isBanned,setIsBanned] = useState(false)
+  const [value, setValue] = useState(data);
+  const [count, setCount] = useState(0);
+  const [isBanned, setIsBanned] = useState(false);
   const handleFollow = (id) => {
-    setValue(prevValue => 
-      prevValue.map(item => 
-        item.id === id ? {...item, isFollow: !item.isFollow} : item
-      )
+    setValue((prevValue) =>
+      prevValue.map((item) =>
+        item.id === id ? { ...item, isFollow: !item.isFollow } : item,
+      ),
     );
-  }
-  const renderCard =value.map(item => (
- <Card key={item.id} id={item.id} name={item.name} description={item.description} image={item.image} isFollow={item.isFollow} onFollow={handleFollow} />
-  
-)) 
+  };
+  const renderCard = value.map((item) => (
+    <Card
+      key={item.id}
+      id={item.id}
+      name={item.name}
+      description={item.description}
+      image={item.image}
+      isFollow={item.isFollow}
+      onFollow={handleFollow}
+    />
+  ));
   return (
-    <div className="bg-slate-800 text-white h-screen flex flex-col justify-center items-center">
-  <Intermediate/>
-  <div>
-    <h3>Advanced</h3>
-    <Advanced/>
-  </div>
+    <div className="bg-slate-800 text-black gap-4 h-screen flex justify-center items-center">
+      {/* {renderCard} */}
+      <ImageCard/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/ImageCard.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+export default function ImageCard() {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const images = [
+    "https://picsum.photos/id/1/600/400",
+    "https://picsum.photos/id/2/600/400",
+    "https://picsum.photos/id/3/600/400",
+  ];
+
+  const next = () => {
+    setCurrentIndex(
+      (prev) => (prev + 1) % images.length
+    );
+  };
+
+  const prev = () => {
+    setCurrentIndex(
+      (prev) => (prev - 1 + images.length) % images.length
+    );
+  };
+
+  return (
+    <div className="relative w-96 h-64">
+      <div className=" w-full h-full">
+        <div
+          className="flex overflow-hidden h-full transition-transform duration-500 ease-in-out"
+          style={{
+            transform: `translateX(-${currentIndex * 100}%)`,
+          }}
+        >
+          {images.map((img, index) => (
+            <img
+              key={index}
+              src={img}
+              alt=""
+              className="w-full h-full object-cover shrink-0"
+            />
+          ))}
+        </div>
+      </div>
+
+      <button onClick={prev}>Prev</button>
+      <button onClick={next}>Next</button>
+    </div>
+  );
+}
