985888d70e1024429556972ec44e082bf79b1d19
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-14 06:29:34 +0545

    challenge 10 page scolling and showing progress bar done

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
-const contentArea = document.getElementById("contentArea");
-const textCount = document.getElementById("textCount");
-const maxInputLimit = document.getElementById("maxChars");
-const maxCharsDisplay = document.getElementById("maxCharsDisplay");
-
-let maxLimit = 100; // Default max limit
-maxInputLimit.addEventListener("input", () => {
-  maxLimit = parseInt(maxInputLimit.value) || 100;
-  maxCharsDisplay.textContent = maxLimit;
-});
-
-contentArea.addEventListener("input", () => {
-  const content = contentArea.value.slice(0, maxLimit); // Limit the content to maxLimit characters
-  contentArea.value = content;
-  textCount.textContent = content.length;
-});
-
-const clearButton = document.getElementById("clearButton");
-clearButton.addEventListener("click", () => {
-  contentArea.value = "";
-  maxInputLimit.value = "";
-  maxCharsDisplay.textContent = 100;
-  textCount.textContent = 0;
+const progressBar = document.querySelector(".progress-bar");
+window.addEventListener("scroll", () => {
+  const scrollTop = window.scrollY;
+  const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+  const scrollPercent = (scrollTop / docHeight) * 100;
+  progressBar.style.width = `${scrollPercent}%`;
 });
