d086fe72078e47d5e6e33009bec556df572d6339
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-14 18:26:15 +0545

    done with condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
+import { useState } from "react"
 import Card from "./Card"
 
 const data = [{
   id:1,
   name:'Dipesh Chaudhary',
   description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.',
-  image:'https://images.unsplash.com/photo-1773332611612-ffdaa753afb1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3MXx8fGVufDB8fHx8fA%3D%3D'
+  image:'https://images.unsplash.com/photo-1773332611612-ffdaa753afb1?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3MXx8fGVufDB8fHx8fA%3D%3D',
+  isFollow:true
 },{
   id:2,
   name:'Manisha Kumari',
   description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.',
-  image:'https://images.unsplash.com/photo-1773332611573-5e5bfa8e5de5?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3OHx8fGVufDB8fHx8fA%3D%3D'
+  image:'https://images.unsplash.com/photo-1773332611573-5e5bfa8e5de5?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3OHx8fGVufDB8fHx8fA%3D%3D',
+  isFollow:false
 },{
   id:3,
   name:'Sumit Kadel',
   description:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate.',
-  image:'https://plus.unsplash.com/premium_photo-1779372801181-0d00b1448662?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMDJ8fHxlbnwwfHx8fHw%3D' 
+  image:'https://plus.unsplash.com/premium_photo-1779372801181-0d00b1448662?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxMDJ8fHxlbnwwfHx8fHw%3D' ,
+  isFollow:true
 }]
 
 const App = () => {
-  const renderCard =data.map(item => (
- <Card key={item.id} name={item.name} description={item.description} image={item.image} />
+  const [value,setValue] = useState(data)
+  const handleFollow = (id) => {
+    setValue(prevValue => 
+      prevValue.map(item => 
+        item.id === id ? {...item, isFollow: !item.isFollow} : item
+      )
+    );
+  }
+  const renderCard =value.map(item => (
+ <Card key={item.id} id={item.id} name={item.name} description={item.description} image={item.image} isFollow={item.isFollow} onFollow={handleFollow} />
   
 )) 
   return (
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,5 +1,10 @@
 
-const Card = ({ name, description, image }) => {
+const Card = ({ name, description, image,isFollow,onFollow,id }) => {
+
+  function handleFollow() {
+    // Logic to follow/unfollow the user
+    onFollow(id);
+  }
   return (
     <div className="w-80 rounded  bg-slate-200">
       <div className="w-full h-65 ">
@@ -8,6 +13,10 @@ const Card = ({ name, description, image }) => {
       <div className="p-2">
         <h2 className="text-xl font-bold uppercase">{name}</h2>
         <p className="text-gray-600">{description}</p>
+        <button className="px-3.5 py-2 bg-blue-500 rounded text-white mt-4">Know more</button>
+        <button className={`px-3.5 py-2 rounded text-white mt-4 ${isFollow ? 'bg-red-500' : 'bg-green-500'}`} onClick={handleFollow}>
+          {isFollow ? 'Unfollow' : 'Follow'}
+        </button>
       </div>
     </div>
   )
