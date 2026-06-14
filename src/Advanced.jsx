aaa8a4f749ff5b13b2609d0400a1f7f8be7ea26c
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-14 19:54:52 +0545

    know about removing items from array

--- src/Advanced.jsx
+++ src/Advanced.jsx
@@ -18,22 +18,7 @@ const Advanced = () => {
   //   },[])
   return (
     <div className="w-98 bg-slate-300 rounded p-4 text-black">
-      <h1
-        className={`text-2xl font-bold ${person.isBanned ? 'text-red-500' : 'text-green-500'}`}
-      >
-        {person.name} is {person.isBanned ? 'banned' : 'not banned'}
-      </h1>
-      <h2>Age is {person.age}</h2>
-      <h2>Gender is {person.gender}</h2>
-      <button onClick={addGender}>Add gender</button>
-      <button
-        className={`px-2.5 py-2 ${person.isBanned ? 'bg-green-600' : 'bg-red-500'} text-slate-200 rounded`}
-        onClick={() =>
-          setPerson((prev) => ({ ...prev, isBanned: !prev.isBanned }))
-        }
-      >
-        {person.isBanned ? 'Unban' : 'Ban'}
-      </button>
+     
 
       <hr />
 
@@ -49,7 +34,7 @@ const Advanced = () => {
         })}
         <button
           onClick={() =>
-            setNumbers((prev) => prev.filter((num, index) => index !== prev.length - 1))
+            setNumbers((prev) => prev.filter((num, index) => num%2!==0))
           }
         >
           erase
