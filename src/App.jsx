4740c1c7a98a81dd97035be1fa3b9b4f7d25c9a4
Dipesh Chaudhary <babasarkar260@gmail.com>
2026-06-15 05:52:12 +0545

    apply rhk

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,21 @@
-import { useState } from 'react';
-import Playlist from './Playlist';
+import { useForm } from 'react-hook-form'
 
 const App = () => {
-  const [playlists, setPlaylists] = useState([
-    { id: 1, name: 'chand tare', singer: 'Kumar',isFav:false },
-    { id: 2, name: 'Tere Naam', singer: 'Udit Narayan',isFav:false },
-    { id: 3, name: 'Tere surror', singer: 'Himesh Resahmaya',isFav:false },
-    { id: 4, name: 'Hamari Adhuri Kahani', singer: 'Arman Malik' ,isFav:false},
-  ]);
-const addFav = (id)=>{
-  setPlaylists(prev=>prev.map(p=>p.id===id?({...p,isFav:!p.isFav}):p))
-}
-const numsFav = playlists.filter(p=>p.isFav).length
-  const showPlaylist = playlists.map((p) => <Playlist key={p.id} song={p} addFav={addFav} />);
+  const {register, handleSubmit} = useForm()
 
+  const submit = (data) => {
+    console.log(data)
+  }
   return (
-    <div className="flex h-screen max-w-7xl mx-auto flex-col items-center justify-center ">
-      <header className="flex justify-between w-full mb-20 ">
-        <div>Orange</div>
-        <div>
-          favorites: <span>{numsFav}</span>
-        </div>
-      </header>
-      <div className='flex gap-4'>{showPlaylist}</div>
+    <div>
+    
+    <form action="" onSubmit={handleSubmit(submit)}>
+      <input type="text" {...register("name")} />
+      <input type="text" {...register("email")} />
+      <button>submit</button>
+    </form>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
